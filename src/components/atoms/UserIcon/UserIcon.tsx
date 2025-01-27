2633f967d02105b5752b7221c5650407ad1d7d05
Chrixu97 <chrix.u97@gmail.com>
2025-01-27 18:09:04 +0100

    Add new Icon components for Activities and Users

---
+++ src/components/atoms/UserIcon/UserIcon.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './UserIcon.scss';
+
+interface UserIconProps {
+  name: string;
+}
+
+const ActivityIcon: React.FC<UserIconProps> = ({ name }) => {
+  const getInitials = (fullName: string) => {
+    const [firstName, lastName] = fullName.split(' ');
+    const firstInitial = firstName ? firstName.charAt(0) : '';
+    const lastInitial = lastName ? lastName.charAt(0) : '';
+    return `${firstInitial}${lastInitial}`;
+  };
+
+  const initials = getInitials(name);
+
+  return (
+    <div className={'user-icon'}>
+      <span>{initials}</span>
+    </div>
+  );
+};
+
+export default ActivityIcon;
