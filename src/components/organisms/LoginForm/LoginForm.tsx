afa36bdcb920de0d6179ac75e747e11220a1facd
Chrixu97 <chrix.u97@gmail.com>
2025-01-28 15:16:19 +0100

    Revert "Add form validation for Login"
    This reverts commit 7a69e4c3230d2770f4c52b4a29547011825d16c9.

--- src/components/organisms/LoginForm/LoginForm.tsx
+++ src/components/organisms/LoginForm/LoginForm.tsx
@@ -1,62 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Input from '@/components/atoms/Input/Input';
 import Button from '@/components/atoms/Button/Button';
 import Info from '@/components/atoms/Info/Info';
-import useFormValidation from '@/hooks/useFormValidation';
 
 import './LoginForm.scss';
 
 const LoginForm: React.FC = () => {
-  interface UseFormValidationReturn {
-    email: string;
-    emailError: boolean;
-    password: string;
-    passwordError: boolean;
-    repeatPassword: string;
-    repeatPasswordError: boolean;
-    handleEmailChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    handlePasswordChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    handleRepeatPasswordChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  }
-
-  const formValidation: UseFormValidationReturn = useFormValidation();
-
   return (
     <form className='login-form'>
 
       <p className='login-form__register-text'>Crea tu cuenta de manera gratuita</p>
 
       <Input
-        type="email"
+        type="text"
         variant="email"
         placeholder="Correo electrónico"
-        value={formValidation.email}
-        onChange={formValidation.handleEmailChange}
-        error={!!formValidation.emailError}
+        error={false}
       />
 
       <Input
         type="password"
         variant="password"
         placeholder="Contraseña"
-        value={formValidation.password}
-        onChange={formValidation.handlePasswordChange}
-        error={!!formValidation.passwordError}
+        error={false}
       />
 
       <Input
         type="password"
         variant="password"
         placeholder="Repetir contraseña"
-        value={formValidation.repeatPassword}
-        onChange={formValidation.handleRepeatPasswordChange}
-        error={!!formValidation.repeatPasswordError}
+        error={false}
       />
 
       <Info variant="error" message="Usuario o contraseña incorrecta" />
-      {formValidation.emailError && <Info variant="error" message="Formato de correo electrónico incorrecto" />}
-      {formValidation.passwordError && <Info variant="error" message="La contraseña debe tener al menos 8 caracteres e incluir: una letra mayúscula, un número y un símbolo especial (como @, #, $, %, etc.)." />}
-      {formValidation.repeatPasswordError &&<Info variant="error" message="La repetición de la contraseña no coincide" />}
+      <Info variant="error" message="Formato de correo electrónico incorrecto" />
+      <Info variant="error" message="La contraseña debe tener al menos 8 caracteres e incluir: una letra mayúscula, un número y un símbolo especial (como @, #, $, %, etc.)." />
+      <Info variant="error" message="La repetición de la contraseña no coincide" />
 
       <Button variant="primary">Iniciar sesión</Button>
       <Button variant="tertiary">Crear una cuenta</Button>
